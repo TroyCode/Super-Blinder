9c15ddf5ea7adff410523e2f2a39419014e25cc7
Troy Wu <pightmp@gmail.com>
2015-10-23 12:57:02 +0800

    add audio_list to youtubePafy, working on audioSegment

--- app/audioSegment.py
+++ app/audioSegment.py
@@ -1,19 +1,20 @@
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
-sound = AudioSegment.from_wav("out.wav")
+def cut(import_path, output_directory=None, output_name='chunk'):
+  sound = AudioSegment.from_wav(import_path)
 
-chunks = split_on_silence(sound, 
+  chunks = split_on_silence(sound, 
     # must be silent for at least half a second
     min_silence_len = 500,
 
     # consider it silent if quieter than -50 dBFS
     silence_thresh = -50
-)
+  )
 
-# for the output filename
-i = 0
-for i, chunk in enumerate(chunks):
-    chunk.export("chunk{0}.wav".format(i), format = "wav")
+  # i for the output filename
+  i = 0
+  for i, chunk in enumerate(chunks):
+    chunk.export("chunk{0}.wav".format(i), format="wav")
 
-print 'There are splited into {number} files'.format(number = i + 1)
+  print 'There are splited into {number} files'.format(number=i + 1)
