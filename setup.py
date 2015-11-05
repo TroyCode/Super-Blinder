96e7c54906ffd720c172191eb0aea872f3e49b3a
Troy Wu <pightmp@gmail.com>
2015-11-05 13:00:56 +0800

    complete setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
-MySQL-python
-google-api-python-client
-pafy
+import os
+
+command = 'sudo pip install --upgrade {package}'.format(
+  package="MySQL-python \
+           google-api-python-client \
+           pafy \
+           pydub" 
+  )
+os.system(command)
